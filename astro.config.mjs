f071b9f73e6de143cd3181bc468da62523d8cfe1
Philippe Malo <philippe.malo21@gmail.com>
2023-01-16 23:06:33 -0500

    Babar wip

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react()],
+  output: "server",
+  adapter: vercel(),
+});
---
+++ src/components/Intro.jsx
@@ -0,0 +1,12 @@
+import "../styles/intro.css";
+import introLogo from "/babar_04_clear.svg";
+
+const Intro = () => {
+  return (
+    <div id="loader">
+      <img src={introLogo} />
+    </div>
+  );
+};
+
+export default Intro;
